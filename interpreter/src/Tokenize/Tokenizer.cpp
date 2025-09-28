ea064ce2b366cbc9d2e90d7cf2845e3a642baf5c
Stig Haag Brommann <stihaabr@gmail.com>
2025-09-28 16:07:15 +0200

    Implementation of LET statement

--- interpreter/src/Tokenize/Tokenizer.cpp
+++ interpreter/src/Tokenize/Tokenizer.cpp
@@ -66,27 +66,28 @@ std::vector<std::string> Tokenizer::ReadLines(std::vector<std::string> lines)
 
 bool Tokenizer::ReadTokens(std::vector<std::string> tokens)
 {
-	for (const auto& token : tokens)
+	for (size_t i = 0; i < tokens.size(); ++i)
 	{
+		const auto& token = tokens[i];
 		if (token == "PRINT")
 		{
-			// std::cout << "PRINT STATEMENT FOUND" << std::endl;
 			Executor::print(m_Strings[0]);
 		}
 		else if (token == "END")
 		{
-			// std::cout << "END STATEMENT FOUND" << std::endl;
 			Executor::end();
 		}
 		else if (token == "LET")
 		{
-			// start of variable found
-			std::cout << "START OF VARIABLE FOUND" << std::endl;
+			if (i + 2 < tokens.size())
+			{
+				std::string varName = tokens[i + 1];
+				std::string varValue = tokens[i + 2];
+				Executor::setVariable(varName, varValue);
+				i += 2;
+			}
 		}
-		else
-		{
-			// No action for other tokens
-		}
+		// ... other tokens
 	}
 	return true;
 }
